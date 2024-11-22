0230c1b8a15d32baaeb9efe2f1dee0aa604235df
Michael <michael.hladky@push-based.io>
2024-11-22 20:00:15 +0100

    wip

--- src/ssr.polyfill.ts
+++ src/ssr.polyfill.ts
@@ -3,16 +3,19 @@ import * as domino from 'domino';
 /**
  * This is a shim to run Angular SSR with 3rd parties that include direct access of Browser APIs at run time as well as in side effects.
  */
+const _dom = domino.createWindow('<!DOCTYPE html><p>Hello world</p>');
+const dom = () => (domino as any).createWindow('<!DOCTYPE html><p>Hello world</p>');
 
 // This enables window access e.g.: window.document;
 // As Angular's server-side polyfills do not include window usage, on a global level, but rather only isolated over Angular DI.
 if (typeof global.window === 'undefined') {
-  (global as any).window = (domino as any).createWindow('<!DOCTYPE html><p>Hello world</p>');
+  (global as any).window = dom();
 }
 
 // This enables document access e.g.: document.createElement('div');
 // As Angular's server-side polyfills do not include document usage, on a global level, but rather only isolated over Angular DI.
 if (typeof global.document === 'undefined') {
-  (global as any).document = ((global as any)?.window ?? (domino as any).createWindow('<!DOCTYPE html><p>Hello world</p>')).document;
+  (global as any).document = ((global as any)?.window ?? dom()).document;
+  // @TODO mock document.cookie.match implementation
 }
 
